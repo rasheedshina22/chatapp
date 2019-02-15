95199617c9fd2c66dc6400b3b050f801a01afb6d
Creig Fortune <fortunephiri004@gmail.com>
2019-02-15 16:48:29 -0500

    final submission

--- labtest/question3/q3.js
+++ labtest/question3/q3.js
@@ -1,10 +1,12 @@
-const delayedTimer =(num)=>{
-    return new Promise((resolve, reject)=>{setTimeout(()=>{
-        resolve(num * num)
-    },500)}
-    )
-}
 
-console.log(delayedTimer())
+const delayedTimer = new Promise((resolve, reject)=>{
+        setTimeout(resolve, 500,(6 * 6)) 
+})
 
+const delayedTimer2 = new Promise((resolve, reject)=>{
+    setTimeout(resolve, 500,(7*7)) 
+})
+
+var promises =Promise.all([delayedTimer,delayedTimer2])
+promises.then((values)=>console.log(values))
 
