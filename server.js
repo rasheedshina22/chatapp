e45846c32bc9475ddd05287eaa2a28198a226bcc
Creig Fortune <fortunephiri004@gmail.com>
2019-03-04 17:34:52 -0500

    minor change

--- server.js
+++ server.js
@@ -28,9 +28,9 @@ io.on('connection',(socket)=>{
 })
 
 //establishing the port
-const port = process.env.PORT || 3000;
+const port = (process.env.PORT|| 3000);
 
-server.listen(3000,()=>{
+server.listen(port,()=>{
     console.log(`now listening on port ${port}`);
 
 });
