8393b139bc7f07392d788ff8633b51328d7221d1
Creig Fortune <fortunephiri004@gmail.com>
2019-06-16 01:27:17 -0400

    update to message component

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const Message =(props)=>{
-      const currentUser = props.currentUser;
+      const currentUser = props.currentUser === props.username ? "Me" : null;
          return (
-            <div className="message" style={currentUser === props.username ? {paddingLeft: 20}: {}}>
+            <div className="message" style={[{paddingBottom:20},currentUser ? {paddingLeft: 25}: {}]}>
               <div className="message-username">
-                 {props.username}
+                 {currentUser || props.username}
               </div>
-              <div className="message-text" style={currentUser === props.username ? {backgroundColor:"#B4B7BA"}: {}}>
+              <div className="message-text" style={currentUser ? {backgroundColor:"#B4B7BA"}: {}}>
                  {props.text}
               </div>
              </div>
