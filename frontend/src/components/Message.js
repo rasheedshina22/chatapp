c0abe988e35ef8a10110c306073de7fc71276d88
Creig Fortune <fortunephiri004@gmail.com>
2019-09-23 21:24:23 -0600

    minor refactor

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -5,12 +5,12 @@ const Message = props => {
   return (
     <div className="message" style={currentUser ? {border:0,justifyItems:"center",color:"#FFC42D",background:"#FFC42D",borderRight: "5px solid #fffffa", marginRight:15} : {}}>
       <div className="message-username"
-        style={currentUser?{fontWeight:"bold", color:"#002343" }:{}}
+        style={currentUser?{fontSize:16, color:"#002343" }:{}}
       >
         {currentUser || props.username}</div>
       <div
         className="message-text"
-        style={{color:"#002343"}}
+        style={currentUser?{color:"#002343"}:{}}
         >
         {props.text}
       </div>
