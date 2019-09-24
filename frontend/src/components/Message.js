185c15784e7094ce7fee896ae42a247282ade838
Creig Fortune <fortunephiri004@gmail.com>
2019-09-23 21:33:03 -0600

    update to css

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -3,7 +3,7 @@ import React from "react";
 const Message = props => {
   const currentUser = props.currentUser === props.username ? "Me" : null;
   return (
-    <div className="message" style={currentUser ? {border:0,justifyItems:"center",color:"#FFC42D",background:"#FFC42D",borderRight: "5px solid #fffffa", marginRight:15} : {}}>
+    <div className="message" style={currentUser ? {border:0,justifyItems:"center",color:"#FFC42D",background:"#FFC42D",borderRight: "5px solid #002343", marginRight:15} : {}}>
       <div className="message-username"
         style={currentUser?{fontSize:16, color:"#002343" }:{}}
       >
