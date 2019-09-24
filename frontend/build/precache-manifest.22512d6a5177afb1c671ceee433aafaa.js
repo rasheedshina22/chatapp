de302236ce381bed520a7faee71bc6d1c0e22c9b
Creig Fortune <fortunephiri004@gmail.com>
2019-09-24 11:05:47 -0600

    minor update to css

--- frontend/build/precache-manifest.22512d6a5177afb1c671ceee433aafaa.js
+++ frontend/build/precache-manifest.22512d6a5177afb1c671ceee433aafaa.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "0fae2f9ab6e9b57158e4",
-    "url": "/static/js/main.2891ec4a.chunk.js"
+    "revision": "e68ff7643f732f953b4c",
+    "url": "/static/js/main.8e9ea232.chunk.js"
   },
   {
     "revision": "28846e9194dbedee83a9",
     "url": "/static/js/2.ef8e75f3.chunk.js"
   },
   {
-    "revision": "0fae2f9ab6e9b57158e4",
-    "url": "/static/css/main.1f7c764a.chunk.css"
+    "revision": "e68ff7643f732f953b4c",
+    "url": "/static/css/main.c2bcf655.chunk.css"
   },
   {
-    "revision": "479bc055a67379d3309592957ecb5453",
+    "revision": "594a4eee13533e4d0166a9f4fd17e671",
     "url": "/index.html"
   }
 ];
--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -3,15 +3,13 @@ import React from "react";
 const Message = props => {
   const currentUser = props.currentUser === props.username ? "Me" : null;
   return (
-    <div className="message" style={currentUser ? {border:0,justifyItems:"center",color:"#FFC42D",background:"#FFC42D",borderRight: "5px solid #002343", marginRight:15} : {}}>
+    <div className="message" style={{marginLeft:"30px"}}>
       <div className="message-username"
-        style={currentUser?{fontSize:16, color:"#002343" }:{}}
-      >
+        style={currentUser ? { background:"#ccc", borderRight:"2px solid #FFC42D"}:{}}
+        >
         {currentUser || props.username}</div>
       <div
-        className="message-text"
-        style={currentUser?{color:"#002343"}:{}}
-        >
+        className="message-text">
         {props.text}
       </div>
     </div>
