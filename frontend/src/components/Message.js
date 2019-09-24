fe0c3abef9df0c7d622a0ee6969c02948507618b
Creig Fortune <fortunephiri004@gmail.com>
2019-09-23 20:21:18 -0600

    cosmetic change

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -3,12 +3,14 @@ import React from "react";
 const Message = props => {
   const currentUser = props.currentUser === props.username ? "Me" : null;
   return (
-    <div className="message" style={currentUser ? { paddingLeft: 35 } : {}}>
-      <div className="message-username">{currentUser || props.username}</div>
+    <div className="message" style={currentUser ? {border:0,justifyItems:"center",color:"#FFC42D",boxShadow:"0.5px 0.8px 5px rgba(255, 196, 45, 0.4)"} : {}}>
+      <div className="message-username"
+        style={currentUser?{fontSize:16, color:"#FFC42D" }:{}}
+      >
+        {currentUser || props.username}</div>
       <div
         className="message-text"
-        style={currentUser ? { backgroundColor: "#e1e6e8" } : {}}
-      >
+        >
         {props.text}
       </div>
     </div>
