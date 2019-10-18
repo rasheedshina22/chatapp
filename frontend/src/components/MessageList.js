45dd8cd7f0166329f7178eab822a6139095483ec
Creig Fortune <fortunephiri004@gmail.com>
2019-10-17 19:19:16 -0600

    update to scroll to last element

--- frontend/src/components/MessageList.js
+++ frontend/src/components/MessageList.js
@@ -1,21 +1,38 @@
 import React from "react";
 import { Message } from "../components";
 
-const MessageList =(props)=> {
+class MessageList extends React.Component{
+  constructor(props){
+    super(props)
+    this.element = React.createRef()
+  }
+  componentDidMount(){
+    this.scrollToBottom()
+  }
+
+  componentDidUpdate(){
+      this.scrollToBottom()
+  }
+  scrollToBottom=()=>{
+    this.element.scrollIntoView({behavior: "smooth", block:"end"})
+  }
+  render(){
     return (
       <div className="message-list">
-        {props.messageData.map((message, index) => {
+        {this.props.messageData.map((message, index) => {
           return (
             <Message
               key={index}
               text={message.chat}
               username={message.name}
-              currentUser={props.currentUser}
+              currentUser={this.props.currentUser}
             />
           );
-        })}
+        })} 
+        <div ref={el=>{this.element = el}}></div>
       </div>
     )
+  }
 }
 
 export { MessageList };
