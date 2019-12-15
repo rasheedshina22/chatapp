10383699f745f5b943e0936385b767c5d8e91501
Creig Fortune <fortunephiri004@gmail.com>
2019-12-15 00:34:58 -0700

    update to message_username

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -4,7 +4,7 @@ const Message = props => {
   const currentUser = props.currentUser === props.username ? "Me" : null;
   return (
     <div className="message" style={currentUser?{marginLeft:"30px"}:{}}>
-      <div className="message-username" style={currentUser?{fontWeight:"bold", fontSize: 13}:{}}>
+      <div className="message-username" style={currentUser?{fontWeight:"bold", fontSize: 13, color:"var(--highlight-color)"}:{}}>
         {currentUser || props.username}</div>
       <div className="message-text" >
         {props.text}
